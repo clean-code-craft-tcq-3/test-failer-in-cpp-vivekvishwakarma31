6277d4b16bfad201a9530c9fbac3551990ac6a4d
vivek Vishwakarma <42151920+vivekvishwakarma31@users.noreply.github.com>
2022-05-02 18:08:22 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -16,6 +16,7 @@ int printColorMap() {
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    assert(result == 0);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
